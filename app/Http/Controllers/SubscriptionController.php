ea6f5bd52851347916d24a888e4a790e8e971ca3
sergeevich ruslan <abishev2012@gmail.com>
2024-02-09 23:15:52 +0500

    form restile

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -14,6 +14,7 @@ class SubscriptionController extends Controller
 
     public function store(Request $request)
     {
+        
         $request->validate([
             'email' => 'required|email|unique:subscriptions',
         ]);
